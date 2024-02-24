3662c1df1dc2d27b9f0cee98a9a1e60a24d663d4
raghadafghani <105122460+raghadafghani@users.noreply.github.com>
2024-02-24 17:43:46 +0200

    Delete meds.c

--- meds.c
+++
@@ -1,3 +0,0 @@
-Azimex 500:Azithromycin:Antibiotic:pneumonia,tonsillitis,skin allergy:3/2025:7:birzeit pharmaceutical company
-Azicare 500:Azithromycin:Antibiotic:pain,tonsillitis,skin allergy:3/2025:11:Pharmacare PLC
-Paramol:Paracetamol:Pain killer:fever,pain:3/2026:9:Beit Jala Pharmaceutical
